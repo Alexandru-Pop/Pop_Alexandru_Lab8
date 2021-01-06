aaa5f71ccad0b4c279c24374e1f119ffc4ce446e
Alexandru Pop <pop.alexandru@hotmail.com>
2021-01-06 16:10:15 +0200

    Add project files.

---
+++ Data/Pop_Alexandru_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pop_Alexandru_Lab8.Models;
+
+namespace Pop_Alexandru_Lab8.Data
+{
+    public class Pop_Alexandru_Lab8Context : DbContext
+    {
+        public Pop_Alexandru_Lab8Context (DbContextOptions<Pop_Alexandru_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pop_Alexandru_Lab8.Models.Book> Book { get; set; }
+    }
+}
